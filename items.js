b4be6dc7fe2be39a2fafd81063410f6e3eedf81d
alibest-ru <56222997+alibest-ru@users.noreply.github.com>
2019-10-06 14:20:07 +0200

    Add files via upload

---
+++ items.js
@@ -0,0 +1,21 @@
+var items = [{
+    name: "item 1",
+    image: "https://via.placeholder.com/150",
+    link: "https://via.placeholder.com/150",
+    id: 0
+}, {
+    name: "item 2",
+    image: "https://via.placeholder.com/150",
+    link: "https://via.placeholder.com/150",
+    id: 2
+}, {
+    name: "cool",
+    image: "https://via.placeholder.com/150",
+    link: "https://via.placeholder.com/150",
+    id: 3
+}, {
+    name: "item 4",
+    image: "https://via.placeholder.com/150",
+    link: "https://via.placeholder.com/150",
+    id: 5
+}]
