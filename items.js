ba76bab97856e3910ea4838acdd5cc12cf2230bf
alibest-ru <56222997+alibest-ru@users.noreply.github.com>
2019-10-07 17:16:28 +0200

    Add files via upload

--- items.js
+++ items.js
@@ -1,5 +1,17 @@
-var items = [{
-    name: "Autumun Office Skirt + Suit",
-    link: "http://s.click.aliexpress.com/e/n9ImRHfi",
-    id: 1
-}]
+var items = [
+    // {
+    //     name: "",
+    //     link: "",
+    //     id: 
+    // },
+     {
+        name: "Autumn/Winter Korean Style Sweater",
+        link: "http://s.click.aliexpress.com/e/4bxWMNRw",
+        id: 2
+    },
+    {
+        name: "Autumun Office Skirt + Suit",
+        link: "http://s.click.aliexpress.com/e/n9ImRHfi",
+        id: 1
+    }
+]
